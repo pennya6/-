5a507babcd9070749defb913607e8edb287d0d31
kimsiyeon <pennya6@hs.ac.kr>
2022-01-04 17:25:01 +0900

    재설정

---
+++ 3_2.py
@@ -0,0 +1,4 @@
+#큰수의 법칙
+#n:배열의 크기, M:숫자회수, K:연속회수
+n,m,k=map(int,input().split())
+data=list(map(int,input().split()))
--- 4_3.py
+++
@@ -1,6 +0,0 @@
-# 구현 - 왕실의 나이트
-# 8*8 좌표 평면
-# 나이트가 이동할 수 있는 경우의수 구하기
-input_data=input()
-row=int(input_data[1])
-columns=int(ord(input_data[0]))-int(ord('a'))+1
