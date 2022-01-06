1dccc31b4a5873eb4c20dc24dabec8966249999f
kimsiyeon <pennya6@hs.ac.kr>
2022-01-06 14:04:34 +0900

    음료수 얼려먹기 dfs 함수 구현

--- 5_3.py
+++ 5_3.py
@@ -1,1 +1,19 @@
 #음료수얼려먹기
+#세로길이 n,가로길이 m
+#접근방법
+#DFS->모든 하위가 1이 나오면 다음으로 넘기기 그리고 카운트 세기
+
+def dfs(graph,v,visited):
+    visited[v]=True
+    print(v,end=' ')
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph,i,visited)
+
+n,m=map(int,input().split())
+graph=[]
+for i in range(n):
+    graph.append(list(map(int,input())))
+print(graph)
+
+
