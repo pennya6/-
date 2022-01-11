4db83501e92616e05a22c0945aaf8644f2e1290e
kimsiyeon <pennya6@hs.ac.kr>
2022-01-11 23:20:56 +0900

    1로 만들기 질문

--- 8_2.py
+++ 8_2.py
@@ -1,4 +1,5 @@
 #1로 만들기
+#다이나믹 프로그래밍은 문제를 풀기전에 도식화 할 필요가 있음 -> 함수호출과정에서 동일하게 여러번 호출하는 함수들이 존재
 x=int(input())
 # while x>=1:
 #     if x==1:
@@ -16,29 +17,18 @@ x=int(input())
 #         x-=1
 #         count += 1
 
-def dynamic(x):
-    count=0
-    if x==1:
-        return count
-    if x%5==0:
-        print(x)
-        count=count+1
-        return dynamic(x/5)
-    elif x%3==0:
-        count += 1
-        return dynamic(x/3)
-    elif x%2==0:
-        count += 1
-        return dynamic(x/2)
-    else:
-        count += 1
-        return dynamic(x+1)
-
 #보텀업 방식
 def bottom_up(x):
     count=0
-    dp=[0]*100
-    dp[0]=1
-    dp[1]=5
+    dp=[0]*30001
+    for i in range(2,x+1):
+        dp[i]=dp[i-1]+1
+        if i%2==0:
+            dp[i]=min(dp[i],dp[i//2]+1)
+        if i%3==0:
+            dp[i]=min(dp[i],dp[i//3]+1)
+        if i%5==0:
+            dp[i]=min(dp[i],dp[i//5]+1)
+    return dp[x]
 
-print(dynamic(x))
+print(bottom_up(x))
