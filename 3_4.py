7b65cc6170346ee48e9024ceafc74d24b8de70d3
kimsiyeon <pennya6@hs.ac.kr>
2022-01-22 00:42:05 +0900

    1이될때까지 완료

---
+++ 3_4.py
@@ -0,0 +1,13 @@
+#1이 될때까지
+#1. -1 / 2. N%K
+n,k=map(int,input().split())
+result=0
+while n>1:
+    if n%k==0:
+        n//=k
+        result+=1
+    else:
+        n-=1
+        result+=1
+
+print(result)
